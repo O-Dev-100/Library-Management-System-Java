617b6ebfe5cd6bfe56588e1cd9fd9360d632f09d
O-DevOps-1 <ofattahserir@gmail.com>
2024-09-09 12:15:18 +0200

    Update Book.java

---
+++ Library-Management-System-Advanced-Java/src/main/java/com/yourname/library/Book.java
@@ -0,0 +1,11 @@
+package com.odev1.library.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+
+    // Getters and setters
+}
